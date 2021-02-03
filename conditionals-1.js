96656f17e267e2579935761f6976aa95c6111625
dnkru <50063920+dnkru@users.noreply.github.com>
2021-02-03 14:57:25 -0500

    Added h1 tag with text, linked script and added pseudocode.

---
+++ conditionals-1.js
@@ -0,0 +1,8 @@
+console.log("Let's do this!")
+/*
+1. Prompt the user for a number between 1 and 10.
+2. Set variable with a number that's based on random number generator.
+3. Check user input with generated random number.
+4. If input is equal to generated random number, alert user that it matches.
+5. If input is not equal to generated number, alert user it does not match.
+*/
