33de30ac6ef3eacd0cb3d26a818a21ab0f0a18f2
Mercy Baffour <mercy.baffour@gmail.com>
2021-02-03 15:11:02 -0500

    Completed Activity 1

--- conditionals-1.js
+++ conditionals-1.js
@@ -6,3 +6,13 @@ console.log("Let's do this!")
 4. If input is equal to generated random number, alert user that it matches.
 5. If input is not equal to generated number, alert user it does not match.
 */
+
+let guess = prompt("Give me a number between 1 and 10.");
+
+let number = Math.floor( Math.random() * 10) + 1;
+
+if (guess == number) {
+    alert(`You guessed the right number...${number}`);
+} else {
+    alert(`Sorry, the right number is...${number}`);
+}
