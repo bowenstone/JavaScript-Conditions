196c7bc785afb023d75a5081a9bb9d31ca7616b9
dnkru <50063920+dnkru@users.noreply.github.com>
2021-02-03 15:41:37 -0500

    Converted psudeocode into actual code.

--- conditionals-2.js
+++ conditionals-2.js
@@ -1,1 +1,11 @@
 console.log("Here we go again...")
+
+let guess = prompt("Give me a number.");
+
+if ((guess % 3 == 0) && (guess % 5 == 0)) {
+    alert('chimichangas!');
+} else if (guess % 3 == 0) {
+    alert('chimi');
+} else if (guess % 5 == 0) {
+    alert(`changas`);
+}
